eb66b5e2573f0b1ee9fcdb5913cadce57e5df2b5
Boletim Nova Casa <actions@github.com>
2026-08-06 00:15:14 +0000

    Boletim atualizado em 05/08/2026 21:15

--- dados.js
+++ dados.js
@@ -1,80 +1,80 @@
 // Gerado automaticamente por atualizar.py - nao edite a mao
 window.DADOS = {
- "atualizado_em": "06/08/2026 00:05",
+ "atualizado_em": "05/08/2026 21:15",
  "manchetes": [
   {
    "titulo": "Novidades e prêmios",
-   "data": "",
+   "data": "05/08/2026",
    "fonte": "Anamaco",
    "resumo": "A palavra de ordem na Construsul é interação. Boa parte dos expositores realiza ações nos estandes durante a feira para atrair os visitantes. As premiações para o público têm dado a tônica do evento. Fora do ambiente...",
    "link": "http://www.revistaanamaco.com.br/novidades-e-premios"
   },
   {
    "titulo": "Espaço Smart tem 1º semestre positivo e espera faturar R$ 1 bilhão em 2026",
-   "data": "",
+   "data": "05/08/2026",
    "fonte": "Anamaco",
    "resumo": "No período em que o mercado tradicional da construção civil caminha em ritmo moderado, a Espaço Smart registrou, no primeiro semestre, faturamento de R$ 550 milhões. Com desempenho acelerado, a empresa caminha a...",
    "link": "http://www.revistaanamaco.com.br/espaco-smart-tem-1-semestre-positivo-e-espera-faturar-r-1-bilhao-em-2026-"
   },
   {
    "titulo": "Reincidência atingiu 85,32% dos consumidores inadimplentes em junho",
-   "data": "",
+   "data": "05/08/2026",
    "fonte": "Anamaco",
    "resumo": "Em junho, o Indicador de Reincidência de Pessoas Físicas, apurado pela CNDL e pelo SPC Brasil, revelou que, do total de negativações, 85,32% foram de devedores reincidentes. O dado refere-se a consumidores que já...",
    "link": "http://www.revistaanamaco.com.br/reincidencia-atingiu-8532-dos-consumidores-inadimplentes-em-junho-"
   },
   {
    "titulo": "Artecola anuncia Ricardo Cappra como seu novo gerente de Marketing",
-   "data": "",
+   "data": "05/08/2026",
    "fonte": "Anamaco",
    "resumo": "Ricardo Luís Cappra acaba de assumir a gerência de Marketing da Artecola. O executivo chega com a missão de fortalecer o posicionamento de marca em diferentes mercados e contribuir para o crescimento dos negócios",
    "link": "http://www.revistaanamaco.com.br/artecola-anuncia-ricardo-cappra-como-seu-novo-gerente-de-marketing-"
   },
   {
    "titulo": "Longeva e relevante",
-   "data": "",
+   "data": "04/08/2026",
    "fonte": "Anamaco",
    "resumo": "Começou hoje e acontece até a próxima sexta-feira, no Centro de Eventos Fiergs, em Porto Alegre (RS), a 27ª edição da Construsul. O evento reúne  mais de 300 expositores e espera receber cerca de 35 mil visitantes...",
    "link": "http://www.revistaanamaco.com.br/longeva-e-relevante"
   },
   {
    "titulo": "Indústria está mais seletiva na diversificação de portfólio, aponta estudo",
-   "data": "",
+   "data": "04/08/2026",
    "fonte": "Anamaco",
    "resumo": "O Índice de Diversificação de Portfólio de Produtos, da Associação Brasileira de Automação-GS1 Brasil, aponta que o setor industrial de transformação brasileiro apresentou um desempenho positivo no primeiro semestre,...",
    "link": "http://www.revistaanamaco.com.br/industria-esta-mais-seletiva-na-diversificacao-de-portfolio-aponta-estudo"
   },
   {
    "titulo": "Construção abriu mais de 168 mil vagas no semestre",
-   "data": "",
+   "data": "04/08/2026",
    "fonte": "Anamaco",
    "resumo": "De acordo com dados do Caged, divulgados pelo Ministério do Trabalho e Emprego, a indústria da construção gerou 14.136 novos empregos em junho no País, o que representa uma alta de 0,46% em relação a maio. Com isso,...",
    "link": "http://www.revistaanamaco.com.br/construcao-abriu-mais-de-168-mil-vagas-no-semestre"
   },
   {
    "titulo": "Casa do Construtor inaugura, em Guaymallén, sua segunda unidade na Argentina",
-   "data": "",
+   "data": "03/08/2026",
    "fonte": "Anamaco",
    "resumo": "A Casa do Construtor acaba de abrir as portas de sua segunda unidade na Argentina. A cidade escolhida foi Guaymallén, na região metropolitana de Mendoza. A nova unidade recebeu investimento de cerca de US$ 100 mil...",
    "link": "http://www.revistaanamaco.com.br/casa-do-construtor-inaugura-em-guaymallen-sua-segunda-unidade-na-argentina-"
   },
   {
    "titulo": "Estudo elege Gerdau como a líder em popularidade digital na indústria do aço no Brasil",
-   "data": "",
+   "data": "03/08/2026",
    "fonte": "Anamaco",
    "resumo": "A Gerdau é a líder em popularidade digital no setor de Siderurgia, Mineração e Metalurgia, de acordo com o estudo inédito “Grandes Marcas Digitais”, realizado pela consultoria de inteligência de dados e pesquisa...",
    "link": "http://www.revistaanamaco.com.br/estudo-elege-gerdau-como-a-lider-em-popularidade-digital-na-industria-do-aco-no-brasil-"
   },
   {
    "titulo": "Ânimo no comércio",
-   "data": "",
+   "data": "03/08/2026",
    "fonte": "Anamaco",
    "resumo": "De acordo com dados apurados pelo Instituto Brasileiro de Economia da Fundação Getulio Vargas, o Índice de Confiança do Comércio subiu 0,4 ponto em julho, para 85,5 pontos. No mês, a confiança oscilou entre os seis...",
    "link": "http://www.revistaanamaco.com.br/animo-no-comercio"
   },
   {
    "titulo": "Terceiro episódio de websérie da Vedacit tem Nicom e Village como protagonistas",
-   "data": "",
+   "data": "02/08/2026",
    "fonte": "Anamaco",
    "resumo": "A Vedacit lançou, em seu canal no Youtube, o terceiro episódio da websérie documental produzida em comemoração aos seus 90 anos. Desta vez, o protagonismo é dos lojistas e parceiros comerciais, representados pela...",
    "link": "http://www.revistaanamaco.com.br/terceiro-episodio-de-webserie-da-vedacit-tem-nicom-e-village-como-protagonistas-"
@@ -86,8 +86,8 @@ window.DADOS = {
    "data": "05/08/2026"
   },
   "selic": {
-   "valor": 14.0,
-   "data": "06/08/2026"
+   "valor": 14.25,
+   "data": "05/08/2026"
   }
  },
  "mensais": {
