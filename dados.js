b2451a12b6e7798cdb5664b21c3b98930927b1f9
Boletim Nova Casa <actions@github.com>
2026-08-26 14:25:53 +0000

    Boletim atualizado em 26/08/2026 11:25

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 // Gerado automaticamente por atualizar.py - nao edite a mao
 window.DADOS = {
- "atualizado_em": "26/08/2026 07:54",
+ "atualizado_em": "26/08/2026 11:25",
  "manchetes": [
   {
    "titulo": "Selic mantida em 14% ao ano",
@@ -11,11 +11,83 @@ window.DADOS = {
    "link": "https://www.bcb.gov.br/controleinflacao/historicotaxasjuros"
   },
   {
+   "titulo": "Casa Mimosa lança projeto para criar conexões fora do ambiente comercial",
+   "data": "26/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "A Casa Mimosa lançou, na noite desta terça-feira (25 de agosto), o  projeto Resenha Mimosa, iniciativa voltada ao fortalecimento do relacionamento B2B e à criação de experiências exclusivas para seus clientes,...",
+   "link": "http://www.revistaanamaco.com.br/casa-mimosa-lanca-projeto-para-criar-conexoes-fora-do-ambiente-comercial-",
+   "aba": "insumos"
+  },
+  {
+   "titulo": "Belgo patrocina CasaCor Minas e apresenta a versatilidade do arame de aço",
+   "data": "26/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "A Belgo Arames participa, pela primeira vez, da CasaCor Minas Gerais, Mostra de arquitetura, design de interiores e paisagismo. Com o tema “Mente e Coração”, a iniciativa propõe uma reflexão sobre o morar como espaço...",
+   "link": "http://www.revistaanamaco.com.br/belgo-patrocina-casacor-minas-e-apresenta-a-versatilidade-do-arame-de-aco-",
+   "aba": "insumos"
+  },
+  {
+   "titulo": "Tatiana Weston Webb é a primeira atleta da plataforma esportiva do Grupo Tigre",
+   "data": "26/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "A Tigre lança o Squad Tigre, plataforma esportiva da marca criada em parceria com o Comitê Olímpico do Brasil como parte de sua estratégia de patrocínio ao esporte ao longo do ciclo olímpico até Los Angeles 2028. A...",
+   "link": "http://www.revistaanamaco.com.br/tatiana-weston-webb-e-a-primeira-atleta-da-plataforma-esportiva-do-grupo-tigre-",
+   "aba": "geral"
+  },
+  {
+   "titulo": "CNA Aponta Escassez de Mão de Obra Como Limite À Produtividade no Agro",
+   "data": "26/08/2026",
+   "fonte": "Forbes",
+   "resumo": "Forbes, a mais conceituada revista de negócios e economia do mundo. Pesquisa com produtores relaciona contratação, qualificação, tecnologia, programas sociais e jornada às mudanças em curso no mercado laboral...",
+   "link": "https://forbes.com.br/forbes-agro/2026/08/cna-aponta-escassez-de-mao-de-obra-como-limite-a-produtividade-no-agro/",
+   "aba": "demanda"
+  },
+  {
    "titulo": "O Próximo Choque da Inflação dos Alimentos nos EUA Está Sendo Construído e a Caminho",
    "data": "26/08/2026",
    "fonte": "Forbes",
    "resumo": "Forbes, a mais conceituada revista de negócios e economia do mundo. Alta dos grãos, clima extremo, guerra no Mar Negro e margens apertadas em restaurantes aumentam o risco de novas pressões sobre custos e preços ao...",
    "link": "https://forbes.com.br/forbes-agro/2026/08/o-proximo-choque-da-inflacao-dos-alimentos-nos-eua-esta-sendo-construido-e-a-caminho/",
+   "aba": "custos"
+  },
+  {
+   "titulo": "Vendas de imóveis avançam 5,4% no primeiro semestre e MCMV registra participação recorde",
+   "data": "26/08/2026",
+   "fonte": "O Empreiteiro",
+   "resumo": "O mercado imobiliário brasileiro encerrou o primeiro semestre de 2026 com crescimento nas vendas de imóveis residenciais novos, mesmo diante de um cenário de juros O post Vendas de imóveis avançam 5,4% no primeiro...",
+   "link": "https://revistaoe.com.br/vendas-de-imoveis-crescem-5-4-no-primeiro-semestre/",
+   "aba": "insumos"
+  },
+  {
+   "titulo": "Fundo imobiliário anuncia amortização de R$ 5,47 por cota; Ifix avança",
+   "data": "26/08/2026",
+   "fonte": "MoneyTimes",
+   "resumo": "O fundo imobiliário Panorama Real Estate Crédito (PNRC11) anunciou uma amortização parcial de suas cotas no valor total de R$ 2,24 milhões, equivalente a R$ 5,47 por cota, segundo fato relevante divulgado ao mercado....",
+   "link": "https://www.moneytimes.com.br/fundo-imobiliario-anuncia-amortizacao-de-r-547-por-cota-ifix-avanca/",
+   "aba": "demanda"
+  },
+  {
+   "titulo": "Piora no humor das famílias",
+   "data": "25/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "O Índice de Confiança do Consumidor, calculado pelo Instituto Brasileiro de Economia da Fundação Getulio Vargas, recuou 3,6 pontos em agosto, para 84,7 pontos, o que representa o menor nível desde novembro de 2022...",
+   "link": "http://www.revistaanamaco.com.br/piora-no-humor-das-familias-",
+   "aba": "demanda"
+  },
+  {
+   "titulo": "Endividamento maior",
+   "data": "25/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "O percentual de famílias brasileiras com dívidas a vencer voltou a crescer e alcançou 82% em julho. O resultado representa um avanço de 0,4 ponto percentual frente a junho (81,6%). O dado é da Pesquisa de...",
+   "link": "http://www.revistaanamaco.com.br/endividamento-maior-",
+   "aba": "geral"
+  },
+  {
+   "titulo": "Abilux e ApexBrasil levarão missão comercial da indústria de iluminação ao Canadá",
+   "data": "25/08/2026",
+   "fonte": "Anamaco",
+   "resumo": "A Abilux em parceria com a ApexBrasil e com o apoio da Secretaria de Comunicação Social do Consulado do Brasil em Toronto promoverão, por meio do projeto de exportação Lux Brasil, a Missão Comercial da Indústria...",
+   "link": "http://www.revistaanamaco.com.br/abilux-e-apexbrasil-levarao-missao-comercial-da-industria-de-iluminacao-ao-canada-",
    "aba": "custos"
   },
   {
@@ -56,22 +128,6 @@ window.DADOS = {
    "fonte": "CBIC",
    "resumo": "A indústria da construção civil segue como um dos principais motores do desenvolvimento econômico no Paraná, liderando a geração de empregos e renda. Com um estoque superior a 180 mil trabalhadores formais no estado...",
    "link": "https://cbic.org.br/sindusconpr-realizou-19a-edicao-do-dia-nacional-da-construcao-social-em-curitiba/",
-   "aba": "demanda"
-  },
-  {
-   "titulo": "Associadas da ABRAMAT premiadas em grande estilo no Prêmio Qualidade Construbusiness 2026",
-   "data": "25/08/2026",
-   "fonte": "ABRAMAT",
-   "resumo": "Promovido pelo SINAPROCIM, pelo SINPROCIM e pelo Instituto ConstruBusiness, o Prêmio Qualidade Construbusiness 2026 reuniu representantes da indústria, de entidades setoriais e do poder público para apresentar dados...",
-   "link": "https://abramat.org.br/associadas-da-abramat-premiadas-no-construbusiness-2026/",
-   "aba": "geral"
-  },
-  {
-   "titulo": "Com primeiro arranha-céu de Sinop em andamento, construtora do Centro-Oeste anuncia R$ 630 milhões em VGV projetados para 2026",
-   "data": "21/08/2026",
-   "fonte": "O Empreiteiro",
-   "resumo": "Em um mercado imobiliário historicamente concentrado no eixo Rio de Janeiro–São Paulo, uma construtora do Centro-Oeste vem desenhando uma trajetória diferente. A São Benedito, fundada O post Com primeiro arranha-céu...",
-   "link": "https://revistaoe.com.br/com-primeiro-arranha-ceu-de-sinop-em-andamento-construtora-do-centro-oeste-anuncia-r-630-milhoes-em-vgv-projetados-para-2026/",
    "aba": "demanda"
   }
  ],
