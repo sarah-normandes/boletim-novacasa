10896aaba0e93a47a8e80fa3ffb717c1ee2c5141
Boletim Nova Casa <actions@github.com>
2026-09-05 22:32:15 +0000

    Boletim atualizado em 05/09/2026 19:32

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 // Gerado automaticamente por atualizar.py - nao edite a mao
 window.DADOS = {
- "atualizado_em": "05/09/2026 16:45",
+ "atualizado_em": "05/09/2026 19:32",
  "manchetes": [
   {
    "titulo": "Selic mantida em 14% ao ano",
